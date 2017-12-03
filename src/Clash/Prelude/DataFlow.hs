b9a72b14b1d172ac02d3d6d306df8230d340bf26
Ben Gamari <ben@smart-cactus.org>
2017-12-03 12:05:55 -0500

    Add lowerDF'

--- src/Clash/Prelude/DataFlow.hs
+++ src/Clash/Prelude/DataFlow.hs
@@ -27,6 +27,7 @@ module Clash.Prelude.DataFlow
     -- * Creating DataFlow circuits
   , liftDF
   , liftDF'
+  , lowerDF'
   , Ready(..)
   , pureDF
   , mealyDF
@@ -149,20 +150,35 @@ liftDF = DF
 data Ready a = Ready | NotReady
              deriving (Eq, Ord, Show)
 
+boolToReady :: Bool -> Ready a
+boolToReady b = if b then Ready else NotReady
+
+validToMaybe :: Bool -> a -> Maybe a
+validToMaybe valid dat
+  | valid     = Just dat
+  | otherwise = Nothing
+
 -- | Like 'liftDF' but with slightly more descriptive types.
 liftDF' :: (Signal dom (Maybe i) -> Signal dom (Ready o)
                          -> (Signal dom (Maybe o), Signal dom (Ready i)))
         -> DataFlow dom Bool Bool i o
 liftDF' f = DF $ \iData iValid oReady ->
-                   let validToMaybe valid dat
-                         | valid     = Just dat
-                         | otherwise = Nothing
-                       boolToReady b = if b then Ready else NotReady
-                       (o, iReady) = f (validToMaybe <$> iValid <*> iData)
+                   let (o, iReady) = f (validToMaybe <$> iValid <*> iData)
                                        (fmap boolToReady oReady)
                        oValid = isJust <$> o
                        oData  = maybe (error "liftDF': invalid output") id <$> o
                    in (oData, oValid, (== Ready) <$> iReady)
+
+-- | Destructure a dataflow circuit.
+lowerDF' :: DataFlow dom Bool Bool i o
+         -> Signal dom (Maybe i)        -- ^ input stream
+         -> Signal dom (Ready o)        -- ^ Outgoing back-pressure
+         -> (Signal dom (Maybe o), Signal dom (Ready i))
+lowerDF' (DF f) i oReady =
+    let (o, oValid, iReady) = f (maybe (error "lowerDF': invalid input") id <$> i)
+                                (isJust <$> i)
+                                ((== Ready) <$> oReady)
+    in (validToMaybe <$> oValid <*> o, boolToReady <$> iReady)
 
 -- | Create a 'DataFlow' circuit where the given function @f@ operates on the
 -- data, and the synchronisation channels are passed unaltered.
